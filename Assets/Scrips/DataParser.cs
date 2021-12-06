d68dacb01695bfb4917f876e0b4c1dcd20bc1a3b
Arseniy <ambelyakov@gmail.com>
2021-12-06 22:12:55 +0300

    add cityParser

--- Assets/Scrips/DataParser.cs
+++ Assets/Scrips/DataParser.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Structure;
 using UnityEngine;
 
-public class DataParser : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Scrips
+{
+    public class DataParser : MonoBehaviour
     {
-        
+        private List<City> _cities = new List<City>();
+
+        public List<City> Cities => _cities;
+
+        void Start()
+        {
+            StreamReader sr = new StreamReader("Assets/Data/Data/Cities.csv");
+            string line = sr.ReadLine();
+            string[] lineAfterSplit = new string[5];
+            while ((line = sr.ReadLine()) != null)
+            {
+                lineAfterSplit = line.Split(',');
+                lineAfterSplit[2] = lineAfterSplit[2].Replace('.', ',');
+                lineAfterSplit[3] = lineAfterSplit[3].Replace('.', ',');
+                _cities = _cities.Append(new City(lineAfterSplit[0],lineAfterSplit[1],double.Parse(lineAfterSplit[2]),double.Parse(lineAfterSplit[3]),lineAfterSplit[4])).ToList();
+            }
+        }
     }
 }
