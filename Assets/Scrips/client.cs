f8f2bd12b5cda19dee9f83ca48573297df4f052b
Arseniy <ambelyakov@gmail.com>
2021-12-10 18:53:28 +0300

    add client

--- Assets/Scrips/client.cs
+++ Assets/Scrips/client.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO;
+using System.Net.Sockets;
+using Structure;
+using UnityEngine.Serialization;
+using Newtonsoft.Json;
 
-public class client : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class client: MonoBehaviour {
+    internal Boolean SocketReady = false;
+    TcpClient _mySocket;
+    NetworkStream _theStream;
+    StreamReader _theReader;
+    String Host = "localhost";
+    Int32 Port = 5050;
+    private string _data;
+    public Graph _graph;
+    void Start () {
+        //SetupSocket();
+        SetupSocket();
+    }
+    void FixedUpdate ()
     {
+        _data = ReadSocket();
+        if (_data != "")
+        {
+            _graph = CreateGraphFromJson(_data);
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public static Graph CreateGraphFromJson(string jsonString)
     {
-        
+        return JsonConvert.DeserializeObject<Graph>(jsonString);
+    }
+    
+    public void SetupSocket() {
+        try {
+            _mySocket = new TcpClient(Host, Port);
+            _theStream = _mySocket.GetStream();
+            _theReader = new StreamReader(_theStream);
+            SocketReady = true;
+        }
+        catch (Exception e) {
+            Debug.Log("Socket error: " + e);
+        }
+    }
+    public String ReadSocket() {
+        if (!SocketReady)
+            return "";
+        if (_theStream.DataAvailable)
+            return _theReader.ReadToEnd();
+        return "";
+    }
+    public void CloseSocket() {
+        if (!SocketReady)
+            return;
+        _theReader.Close();
+        _mySocket.Close();
+        SocketReady = false;
     }
 }
---
+++ Assets/Structure/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Structure
+{
+    [Serializable]
+    public class Graph
+    {
+        public List<List<double>> matrix;
+        public double firstShortestWay;
+        public double secondShortestWay;
+        
+    }
+}
